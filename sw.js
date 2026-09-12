cbb77e694cc03ce3d800b42cca4c265cf945854a
kingstatue <kingstatue@gmail.com>
2026-09-13 00:05:22 +0530

    header block in paste past pipe not needed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'mgm-bca-absentee-informer';
-const CACHE_NAME = 'mgm-bca-absentee-informer-v181-year-aliases';
+const CACHE_NAME = 'mgm-bca-absentee-informer-v182-space-hdr';
 
 const ASSETS_TO_CACHE = [
   './',
